44e0d5b6ae069f923ff20ff98908e6e6f45ff055
sliwa262 <sliwa262@wp.pl>
2017-03-20 10:26:29 +0100

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -18,7 +18,7 @@ UTC_DATE := $(shell date +%s)
 DATE     := $(shell date +%Y%m%d)
 
 CM_BUILDTYPE := EXPERIMENTAL
-CM_EXTRAVERSION := asturel
+CM_EXTRAVERSION := sliwa262
 
 #PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=yakju BUILD_FINGERPRINT="google/yakju/maguro:4.1.1/JRO03C/398337:user/release-keys" PRIVATE_BUILD_DESC="yakju-user 4.1.1 JRO03C 398337 release-keys"
 #PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SLIM_HW_SE BUILD_ID=JRO03C BUILD_FINGERPRINT=Huawei/MediaPad/hws7300u:4.0.3/HuaweiMediaPad/C002B010SP10:user/release-keys PRIVATE_BUILD_DESC="MediaPad-user 4.0.3 HuaweiMediaPad C002B010SP10 release-keys"
